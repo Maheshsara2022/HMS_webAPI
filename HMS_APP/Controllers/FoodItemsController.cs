f4377b3b9aab9db9d2a89865d7b6c6c0a6e933b1
Maheshsara2022 <106724714+Maheshsara2022@users.noreply.github.com>
2022-06-16 12:49:12 -0400

    finalcommit
    final commit

--- HMS_APP/Controllers/FoodItemsController.cs
+++ HMS_APP/Controllers/FoodItemsController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using HMS_DATAACCESS.Database;
+using HMS_Repository.Interface;
+using HMS_Repository.Modals;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,5 +14,29 @@ namespace HMS_APP.Controllers
     [ApiController]
     public class FoodItemsController : ControllerBase
     {
+        private readonly IFoodRepository _FoodRepository;
+        public FoodItemsController(IFoodRepository foodRepository)
+        {
+            this._FoodRepository = foodRepository;
+        }
+        [HttpGet]
+        [Route("GetCheaforders")]
+        public async Task<IList<cheafupdateitemStatus>> GetCheaforders()
+        {
+            return await _FoodRepository.GetCheaforders();
+        }
+        [HttpPost]
+        [Route("updateItemStatus")]
+        public async Task<bool> updateItemStatus(cheafupdateitemStatus itemid)
+        {
+            return await _FoodRepository.updateItemStatus(itemid);
+        }
+
+        [HttpPost]
+        [Route("InserUpdateFoodItem")]
+        public async Task<bool> InserUpdateFoodItem(FoodItem fooddata)
+        {
+            return await _FoodRepository.InserUpdateFoodItem(fooddata);
+        }
     }
 }
--- HMS_Repository/Repository/FoodRepository.cs
+++ HMS_Repository/Repository/FoodRepository.cs
@@ -1,10 +1,82 @@
-﻿using System;
+﻿using HMS_DATAACCESS.Database;
+using HMS_Repository.Interface;
+using HMS_Repository.Modals;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace HMS_Repository.Repository
 {
-    class FoodRepository
+   public class FoodRepository : IFoodRepository
     {
+        private readonly HMSDBDbContext _context;
+
+        public FoodRepository(HMSDBDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IList<cheafupdateitemStatus>> GetCheaforders()
+        {
+            var preparinngitems = (from s in  _context.OrderItemstbls
+                                   join ot in _context.OrderTbls on s.OrderId equals ot.Id
+                                   join t in _context.TableMasters on ot.TableId equals t.Id
+                                   where s.Status =="P"
+                                   orderby s.Id ascending
+                                  select new cheafupdateitemStatus
+                                  { 
+                                      itemID = s.Id,
+                                      itemName = s.ItemName,
+                                      itemstatus=s.Status,
+                                      employeID=ot.EmployeeId,
+                                      orderId=s.OrderId,
+                                      tableID=ot.TableId,
+                                      tablename=t.Name
+                                  }).ToList();
+            return await Task.Run(() => preparinngitems);
+        }
+        public async Task<bool> updateItemStatus(cheafupdateitemStatus itemid)
+        {
+            var orderitems = _context.OrderItemstbls.Where(x => x.Id == itemid.itemID).FirstOrDefault();
+                orderitems.Status = "D";
+                _context.Entry(orderitems).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public Task<bool> InserUpdateFoodItem(FoodItem fooddata)
+        {
+            bool status = false;
+            if (fooddata.Id == 0)
+            {
+                _context.FoodItems.Add(fooddata);
+                _context.SaveChangesAsync();
+                status = true;
+            }
+            else
+            {
+                var getmpdat = _context.FoodItems.Where(u => u.Id == fooddata.Id).FirstOrDefault();
+                if (getmpdat != null)
+                {
+                    getmpdat.ItemName = fooddata.ItemName ?? getmpdat.ItemName;
+                    getmpdat.ItemDec = fooddata.ItemDec ?? getmpdat.ItemDec;
+                    getmpdat.ItemType = fooddata.ItemType ?? getmpdat.ItemType;
+                    getmpdat.Price = fooddata.Price ?? getmpdat.Price;
+                    getmpdat.CategoryId= fooddata.CategoryId ?? getmpdat.CategoryId;
+                   
+                    _context.Entry(getmpdat).State = EntityState.Modified;
+                    _context.SaveChangesAsync();
+                    status = true;
+                }
+                else { status = false; }
+
+            }
+            return Task.Run(() => status);
+
+        }
+
     }
 }
